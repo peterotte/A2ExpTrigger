4fb3932d4932dc5c5925b37c41d039755cb49653
Peter Otte <otte@kph.uni-mainz.de>
2014-01-16 09:46:37 +0100

    Firmware 7c
    Changed signal to CB Slave to L2 Strobe

--- trigger.vhd
+++ trigger.vhd
@@ -41,7 +41,7 @@ architecture RTL of trigger is
 
 	subtype sub_Address is std_logic_vector(11 downto 4);
 	constant BASE_TRIG_FIXED : sub_Address 							:= x"f0" ; -- r
-	constant TRIG_FIXED_Master : std_logic_vector(31 downto 0)  := x"0100007b";
+	constant TRIG_FIXED_Master : std_logic_vector(31 downto 0)  := x"0100007c";
 
 	--Pre L1
 	constant BASE_TRIG_PreTriggerMask : sub_Address								:= x"15"; --r/w
@@ -787,7 +787,7 @@ begin
 	trig_out(1) <= L1Trigger_Gated;
 	trig_out(2) <= ExperimentTrigger; --for TCS
 	trig_out(3) <= L1Trigger2_Gated;
-	trig_out(4) <= L2Trigger;
+	trig_out(4) <= L2_AllORRawTriggers_IntDelayed; --Signal to CBSlave Modules to save Hit Pattern
 	trig_out(10 downto 5) <= (10 downto 5 => '0');
 	trig_out(11) <= ExperimentTrigger; --for TAPS
 	trig_out(12) <= L1Trigger;
